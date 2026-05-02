f9f0aa2fed39a68b0db661a682a49717df1aab68
Benita <theexceptionalpm@gmail.com>
2026-05-02 15:04:43 +0100

    Fix syntax error in CohortDetail and restore admin job buttons

--- src/pages/admin/Academy/CohortDetail.tsx
+++ src/pages/admin/Academy/CohortDetail.tsx
@@ -1577,6 +1577,7 @@ const CohortDetail = () => {
                                     </div>
                                 </motion.div>
                             </div>
+                        )}
                     </TabsContent>
                 </Tabs>
             </div>
